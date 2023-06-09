72f79859d781710f0f0e15cf81baf9af1578a2cc
NamiqMy <namiqmy@code.edu.az>
2023-06-10 01:31:13 +0400

    DateTime project

---
+++ DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Date
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen tarihi 'dd | MM | yyyy' formatında girin:");
+
+            try
+            {
+                string inputDate = Console.ReadLine();
+
+                var date = DateTime.ParseExact(inputDate, "dd/MM/yyyy");
+
+                Console.WriteLine("Girilen tarih: " + date.ToString("dd.MM.yyyy"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
